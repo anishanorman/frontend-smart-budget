b346987fe7949cbadc75552bc37b6942ec3f9ce0
Troy Johnson <Troy.Johnson@hl.co.uk>
2022-12-16 16:42:24 +0000

    new and improved!

--- src/components/forms/IncomeForm.tsx
+++ src/components/forms/IncomeForm.tsx
@@ -1,70 +1,89 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 
-export default function IncomeForm (props: any) {
-  
-  const [profile, setProfile] = useState({
-    revenue_type:"",
-    amount: ""
-  })
- 
+export default function IncomeForm(props: any) {
+  // initialize the state variables for the form data
+  const [formData, setFormData] = useState({
+    revenueType: "",
+    yearlyAmount: "",
+    monthlyAmount: "",
+  });
 
-  const [valid, setValid] = useState(false)
+  // initialize the state variable for the radio button selection
+  const [selectedOption, setSelectedOption] = useState("");
 
+  // handle changes to the form inputs
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     const element: HTMLInputElement = event.target;
     const { name, value } = element;
-    setProfile((prevProfile) => ({
-      ...prevProfile,
-      [name]: value
-    }))
-  }
-  
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-    var postReq = JSON.stringify({
-      revenue_type: profile.revenue_type,
-      amount: profile.amount
-    })
-    // uncomment when hooking up to backend
-    // await fetch("/users", {
-    //   method: "POST",
-    //   body: postReq,
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   }
-    // })
-    console.log(`Sent the following:
-    
-    ${postReq}`)
-    props.onSubmit(true)
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
   }
 
-  useEffect(() => {
-    //set more form validations here if needed
-    if (profile.revenue_type !== "" && profile.amount !== "") {
-      typeof profile.amount === 'number'? setValid(true) : setValid(false)
-    } else {setValid(false)}
-  }, [profile])
-  
-  return(
-      <form id="regForm" onSubmit={handleSubmit}>
-        <h2>Add Revenue</h2>
+  // handle changes to the radio button selection
+  function handleOptionChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setSelectedOption(event.target.value);
+  }
+
+  // handle form submission
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(`Form data: ${JSON.stringify(formData)}`);
+    console.log(`Selected option: ${selectedOption}`);
+    // you can add additional code here to handle the form submission, such as sending the form data to a backend server
+  }
+
+  // render the form
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Income Form</h2>
+      <label>
+        Revenue Type:
         <input
-          value={profile.revenue_type || ""}
+          value={formData.revenueType || ""}
           onChange={handleChange}
-          name="revenue_type"
+          name="revenueType"
           type="text"
-          placeholder="Type of Revenue"
+          placeholder="Enter Revenue Type"
         />
-        <br />
+      </label>
+      <br />
+      <label>
         <input
-          value={profile.amount || ""}
-          onChange={handleChange}
-          type="number"
-          name="amount"
-          placeholder="Enter The Monthly Amount"
+          type="radio"
+          value="yearlyAmount"
+          checked={selectedOption === "yearlyAmount"}
+          onChange={handleOptionChange}
         />
-        <button disabled={!valid} type="submit">Submit</button>
-      </form>
-  )
+        Yearly Amount:
+        <input
+          value={formData.yearlyAmount || ""}
+          onChange={handleChange}
+          name="yearlyAmount"
+          type="number"
+          placeholder="Enter Yearly Amount"
+        />
+      </label>
+      <br />
+      <label>
+        <input
+          type="radio"
+          value="monthlyAmount"
+          checked={selectedOption === "monthlyAmount"}
+          onChange={handleOptionChange}
+        />
+        Monthly Amount:
+        <input
+          value={formData.monthlyAmount || ""}
+          onChange={handleChange}
+          name="monthlyAmount"
+          type="number"
+          placeholder="Enter Monthly Amount"
+        />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
--- src/components/forms/RegisterForm.tsx
+++ src/components/forms/RegisterForm.tsx
@@ -1,96 +1,134 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 
 export default function RegisterForm(props: any) {
-
-  const [profile, setProfile] = useState({
+  const [formData, setFormData] = useState({
     username: "",
     email: "",
+    mobile: "",
+    password: "",
     city: "",
-    createPassword: "",
-    repeatPassword: "",
-  })
+    county: "",
+  });
 
-  const [valid, setValid] = useState(false)
+  // initialize the state variable for the button useability
+  const [valid, setValid] = useState(false);
+  // initialize the state variable for password strength 
+  const [passwordStrength, setPasswordStrength] = useState("");
 
+  // handle changes to the form inputs
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     const element: HTMLInputElement = event.target;
     const { name, value } = element;
-    setProfile((prevProfile) => ({
-      ...prevProfile,
-      [name]: value
-    }))
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
   }
-  
+
+  // handle form submission
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-    var postReq = JSON.stringify({
-      username: profile.username,
-      email: profile.email,
-      city: profile.city,
-      pw_hash: profile.createPassword
-    })
-    // uncomment when hooking up to backend
-    // await fetch("/users", {
-    //   method: "POST",
-    //   body: postReq,
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   }
-    // })
-    console.log(`Sent the following:
-    
-    ${postReq}`)
-    props.onSubmit(true)
+    event.preventDefault();
+    console.log(`Form data: ${JSON.stringify(formData)}`);
+
+    // you can add additional code here to handle the form submission, such as sending the form data to a backend server
+  }
+
+  // function that determines the strength of the password 
+  function checkPasswordStrength(password: string) {
+    if (password.length < 8) {
+      return "Password Strength: Weak";
+    } else if (password.length >= 8 && password.length < 12) {
+      return "Password Strength: Medium";
+    } else {
+      return "Password Strength: Strong";
+    }
   }
 
   useEffect(() => {
-    //set more form validations here if needed
-    if (profile.username !== "" && profile.email !== "" && profile.city !== "" && profile.createPassword !== "" && profile.repeatPassword !== "") {
-      profile.createPassword === profile.repeatPassword ? setValid(true) : setValid(false)
-    } else {setValid(false)}
-  }, [profile])
-  
-  return(
-      <form id="regForm" onSubmit={handleSubmit}>
-        <h2>Create new account</h2>
-        <input
-          value={profile.username || ""}
-          onChange={handleChange}
-          name="username"
-          type="text"
-          placeholder="Username*"
-        />
-        <input
-          value={profile.email || ""}
-          onChange={handleChange}
-          type="text"
-          name="email"
-          placeholder="Email Address*"
-        />
-        <input
-          value={profile.city || ""}
-          onChange={handleChange}
-          type="text"
-          name="city"
-          placeholder="City*"
-        />
-        <br />
-        <input
-          value={profile.createPassword || ""}
-          onChange={handleChange}
-          type="password"
-          name="createPassword"
-          placeholder="Create Password*"
-        />
-        <input
-          value={profile.repeatPassword || ""}
-          onChange={handleChange}
-          type="password"
-          name="repeatPassword"
-          placeholder="Repeat Password*"
-        />
-        <a href="/login">Already Registered?</a>
-        <button disabled={!valid} type="submit">Submit</button>
+    // Check password strength whenever password changes
+    setPasswordStrength(checkPasswordStrength(formData.password));
+  }, [formData.password]);
+
+  // UseEffect hook  that allows you to perform side effects in function components
+  useEffect(() => {
+    // set more form validations here if needed
+
+    const passwordStrength = checkPasswordStrength(formData.password);
+    if (
+      formData.username !== "" &&
+      formData.email !== "" &&
+      formData.mobile !== "" &&
+      (formData.password !== "" && passwordStrength !== "Password Strength: Weak") &&
+      formData.city !== "" &&
+      formData.county !== "")
+     {
+      setValid(true);
+    } else {
+      setValid(false);
+    }
+  }, [formData]);
+
+  // render the form
+  return (
+    <form id="registerForm" onSubmit={handleSubmit}>
+      <h2>Register</h2>
+      <br />
+      <input
+        value={formData.username || ""}
+        onChange={handleChange}
+        name="username"
+        type="text"
+        placeholder="Enter Username"
+      />
+      <br />
+      <input
+        value={formData.email || ""}
+        onChange={handleChange}
+        name="email"
+        type="email"
+        placeholder="Enter Email"
+      />
+      <br />
+      <input
+        value={formData.mobile || ""}
+        onChange={handleChange}
+        name="mobile"
+        type="mobile"
+        placeholder="Enter Mobile"
+      />
+      <br />
+      <input
+        value={formData.password || ""}
+        onChange={handleChange}
+        name="password"
+        type="password"
+        placeholder="Enter Password"
+      />
+      <br />
+      <p>{passwordStrength}</p>
+      <br />
+      <input
+        value={formData.city || ""}
+        onChange={handleChange}
+        name="city"
+        type="text"
+        placeholder="Enter City"
+      />
+      <br />
+      <input
+      value={formData.county || ""}
+      onChange={handleChange}
+      name="county"
+      type="text"
+      placeholder="Enter County"
+      />
+      <br />
+      <a href="/register">Not Registered?</a>
+      <br />
+      <button disabled={!valid} type="submit">Submit</button>
       </form>
   )
-  }
+}
+      
+
+
