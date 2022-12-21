1475cc2b34b0eee011e442b3f95f456688422f6c
Tres1J <113594434+Tres1J@users.noreply.github.com>
2022-12-21 04:44:20 +0000

    Update Income.tsx

--- src/pages/Income.tsx
+++ src/pages/Income.tsx
@@ -7,6 +7,11 @@ import { useNavigate } from "react-router-dom";
 import Btn from "../components/Btn";
 import {useState} from "react"
 
+//Firstly why is this needed ??????????
+// secondly once agian thank you for telling me but key information concerning form function
+// You put me under review but make key changes like this !?!?!?
+// 👍
+
 export default function Income(props: any) {
 
     const navigate = useNavigate()
